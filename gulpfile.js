e4eeb4ac0a2ee9c71746f5c1e8babaf150fd6a99
Dca <dca35311@gmail.com>
2014-08-22 21:30:58 +0800

    Fix gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
+var gulp = require('gulp');
 var deploy = require("gulp-gh-pages");
 
 gulp.task('deploy', function () {
     gulp.src('./**/*')
         .pipe(deploy());
 });
+
+
+gulp.task('publish', ['deploy']);
